566af354c84cb6be604fed2bf8a349a15df45ef2
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-02-01 18:04:24 +0530

    just finish touch up

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import {Navbar, Hero, Project, Education, Contact} from "./components"
 function App() {
 
   return (
-    <div className='bg-black w-full h-full px-40'>
+    <div className='bg-black w-full h-full px-5 md:w-full md:bg-black md:h-full md:px-10 lg:w-full lg:h-full lg:bg-black lg:px-40'>
       <Navbar/>
       <div>
         <section id='home'><Hero/></section>
         <section id='project'><Project/></section>
         <section id='Education'><Education/></section>
-        <section id='Contact'><Contact/></section>
+        <section id='Contact'><Contact/></section> 
       </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,50 +1,86 @@
 import React from 'react'
 
 function Hero() {
-
   return (
-   <div className='flex justify-between py-20'>
-    <div className='bg-sky-300 rounded-full shadow-blue-50 shadow-2xl transition-transform duration-200  hover:-translate-y-4 cursor-pointer hover:rotate-y-180'>
-        <img src="../../public/pfp.jpg" alt="profile image" width="250px" style={{borderRadius:"50%"}}/>
-    </div>
-     <div className='text-slate-400 flex  flex-col items-center '> 
-        <div className='font-extrabold text-6xl'>
-        Hi, I'm <span className='transition-colors duration-300 hover:bg-[#a855f7] hover:text-white px-1 rounded-2xl'>MANISH</span>
+    <div className='flex flex-col items-center py-8 md:flex-row md:justify-between md:py-10 lg:py-20'>
+
+      <div className='w-[200px] lg:w-[250px] bg-sky-300 rounded-full shadow-2xl  shadow-blue-50 
+        transition-transform duration-200 hover:-translate-y-4 hover:rotate-y-180 cursor-pointer overflow-hidden'>
+        <img 
+          src="/pfp.jpg" 
+          alt="profile image" 
+          className='w-full h-full rounded-full'
+        />
+      </div>
+
+      <div className='text-slate-400 flex flex-col items-center py-8'> 
+
+        <div className='font-extrabold text-4xl sm:text-5xl lg:text-6xl text-center'>
+          Hi, I'm{' '}
+          <span className='transition-colors ease-in duration-300 hover:bg-[#9136e6] hover:text-white px-1 rounded-2xl'>
+            MANISH
+          </span>
         </div>
-        <div className='font-bold text-3xl pt-4 pb-2 text-slate-400 '>
-        <span>A </span> <span className='bg-[#a855f7] text-white px-2  rounded-2xl'>React-JS</span> developer
+
+        <div className='font-bold text-2xl sm:text-3xl pt-4 pb-2 text-slate-400 text-center'>
+          <span>A </span>
+          <span className='bg-[#9136e6] text-white px-2 rounded-2xl'>React-JS</span>
+          {' '}developer
         </div>
-        <div className='text-white text-3xl font-bold pb-2'>
-        building seamless interfaces
-        </div>
-        <div className='text-white text-3xl font-bold'>
+
+        <div className='text-center'>
+          <span className='text-white text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold'>
+            building seamless interfaces
+          </span>
+          <span className='text-white text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold pl-2'>
             and functionalities
+          </span>
         </div>
+
         <div className='py-6'>
-            <button className='py-2 px-6 rounded-full border-slate-500 border-2 text-2xl font-bold text-slate-200 transition-transform duration-300 hover:-translate-y-4  hover:text-sky-300  cursor-pointer shadow shadow-white'
-            >Resume</button>
+          <button className='py-2 px-6 rounded-full border-slate-500 border-2 text-xl sm:text-2xl 
+            font-bold text-slate-200 transition-transform duration-300 hover:-translate-y-4 
+            hover:text-sky-300 cursor-pointer shadow shadow-white'>
+            Resume
+          </button>
         </div>
-        <div className='flex gap-2' >
-            <span 
-            className='border-2 border-slate-500 p-1 rounded-full cursor-pointer transition-transform duration-400 hover:-translate-y-4 shadow-white shadow-2xl hover:rotate-y-180'>
-                <a href="https://github.com/TheManishDaksh" target="_blank" rel="noopener noreferrer">
-                <img src="/github.png" alt="github" width="60px"/>
-                </a>
-            </span>
-            <span className='border-2 border-slate-500 p-1 rounded-full bg-white cursor-pointer transition-transform duration-400 hover:-translate-y-4 shadow-white shadow-2xl hover:rotate-y-180'>
-                <a href="https://x.com/manish_dakshh" target="_blank" rel="noopener noreferrer"> 
-                <img src="/twitter.png" alt="x" width="60px" style={{borderRadius: "60%"}}/>
-                </a>
-            </span>
-          
-            <span className='border-2 border-slate-500 p-1 rounded-full cursor-pointer transition-transform duration-400 hover:-translate-y-4 shadow-white shadow-2xl hover:rotate-y-180'>
-                <a href="https://www.linkedin.com/in/manish-kumar-262846252" target="_blank" rel="noopener noreferrer">
-                <img src="/linkedin.png" alt="linkedIn" width="60px" style={{borderRadius: "50%"}} />
-                </a>
-            </span>
+
+        <div className='flex gap-4'> 
+          <a 
+            href="https://github.com/TheManishDaksh" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className='block border-2 border-slate-500 p-1 rounded-full cursor-pointer 
+              transition-all duration-300 hover:-translate-y-4 shadow-2xl shadow-white 
+              hover:rotate-90'
+          >
+            <img src="/github.png" alt="github" className='w-[40px] sm:w-[60px]'/>
+          </a>
+
+          <a 
+            href="https://x.com/manish_dakshh" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className='block border-2 border-slate-500 p-1 rounded-full bg-white cursor-pointer 
+              transition-all duration-300 hover:-translate-y-4 shadow-2xl shadow-white 
+              hover:rotate-90'
+          > 
+            <img src="/twitter.png" alt="x" className='w-[40px] sm:w-[60px] rounded-[60%]'/>
+          </a>
+
+          <a 
+            href="https://www.linkedin.com/in/manish-kumar-262846252" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className='block border-2 border-slate-500 p-1 rounded-full cursor-pointer 
+              transition-all duration-300 hover:-translate-y-4 shadow-2xl shadow-white 
+              hover:rotate-90'
+          >
+            <img src="/linkedin.png" alt="linkedIn" className='w-[40px] sm:w-[60px] rounded-full'/>
+          </a>
         </div>
+      </div>
     </div>
-   </div>
   )
 }
 
