7bcc77e561e6362d6b29d50e863c2a90ff4a7c85
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-02-10 22:39:46 +0530

    added resume

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import {Navbar, Hero, Project, Education, Contact} from "./components"
 function App() {
 
   return (
-    <div className='bg-black w-full h-full px-5 md:w-full md:bg-black md:h-full md:px-10 lg:w-full lg:h-full lg:bg-black lg:px-40'>
+    <div className='bg-black w-full h-full px-5 md:w-full md:bg-black md:h-full md:px-10 lg:w-full lg:h-full lg:bg-black lg:px-40'
+    style={{
+      backgroundImage: "radial-gradient(circle at 0.5px 0.5px, rgba(6, 182, 212, 0.2) 0.5px, transparent 0)",
+      backgroundSize: "8px 8px",
+      backgroundRepeat: "repeat"
+    }}
+    >
       <Navbar/>
       <div>
         <section id='home'><Hero/></section>
